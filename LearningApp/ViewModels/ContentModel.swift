3b779969504d9c1659548c76b6b8477148eb2065
Siberian tiger <artliza@gmail.com>
2021-11-07 10:00:04 -0500

    -Add assets -Add data files -Add models -Parse json -Parse style

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+
+class ContentModel:ObservableObject {
+    
+    @Published var modules=[Module]()
+    var styleData:Data?
+    
+    init() {
+        
+        getLocalData()
+    }
+    
+    
+    func getLocalData(){
+        
+       // 1. Get URL to the JSON file
+        let jsonUrl=Bundle.main.url(forResource: "data", withExtension: "json")
+        do {
+        //2.Read the file into a data object
+        let jsonData = try Data(contentsOf: jsonUrl!)
+        let jsonDecoder = JSONDecoder()
+            
+          
+           let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+                //Assign parsed modeules to modules property
+                self.modules = modules
+         
+        }
+        catch {
+            print ("Couldn't parse json data")
+        }
+        
+        //Parse the style data
+        let styleUrl=Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+        //Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }
+        catch {
+            print("Couldn't parse style data")
+        }
+    }
+}
